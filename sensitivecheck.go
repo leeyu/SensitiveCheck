413e402ff0ed14da0ee34eb08ef8b34f75f1f7c7
yuleiyu <yuleiyu@trophytech.net>
2016-11-04 20:21:06 +0800

    add controller

--- sensitivecheck.go
+++ sensitivecheck.go
@@ -1,264 +1,295 @@
 package sensitivecheck
 
-import(
-"fmt"
-"bufio"
-"os"
-"strings"
-"net/http"
-"encoding/json"
-"strconv"
-/*
-"bytes"
-*/
-)
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	/*
+	   "bytes"
+	*/)
 
-type node struct{
-    fail *node
-    nc rune
-    sons map[rune] *node
-    count int
-    }
+type node struct {
+	fail  *node
+	nc    rune
+	sons  map[rune]*node
+	count int
+	word  []rune
+}
 
-type resp struct{
-    Errno int
-    Errmsg string
-    Data string
-    }
+type resp struct {
+	Errno  int
+	Errmsg string
+	Data   string
+}
 
 type ColorGroup struct {
-    ID     int
-    Name   string
-    Colors []string
+	ID     int
+	Name   string
+	Colors []string
 }
 
-var root *node
+var Root *node
 
 var errres = resp{
-    Errno: -1,
-    Errmsg: "errno happened",
-    Data: "0",
-    }
-
-func (root *node) Scheck(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-    ct := r.Form["ct"]
-
-    if nil == ct {
-        ret := echojson(errres)
-        fmt.Fprintf(w, "%s", ret)
-        return
-        }
-
-    //    blacklist.GetRoot()
-    //    black_root := blacklist.GetRoot()
-    content := ct[0]
-    cLog.Notice("in scheck" + ct[0])
-
-    if "" == content {
-        ret := echojson(errres)
-        fmt.Fprintf(w, "%s", ret)
-        return
-        }
-
-    cnt := root.Query(content)
-
-    res := resp{
-        Errno: 0,
-        Errmsg: "ok",
-        Data: strconv.Itoa(cnt),
-        }
-
-    ret := echojson(res)
-    fmt.Fprintf(w, "%s", ret)
-//    fmt.Printf("this is :%s", string(ret), res.errmsg, res.errno)
-//    fmt.Fprintf(w, "Catch: %s", cnt)
+	Errno:  -1,
+	Errmsg: "errno happened",
+	Data:   "0",
 }
 
+func (Root *node) Scheck(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	ct := r.Form["ct"]
+
+	if nil == ct {
+		ret := echojson(errres)
+		fmt.Fprintf(w, "Catch: %s", ret)
+		return
+	}
+
+	//    blacklist.GetRoot()
+	//    black_Root := blacklist.GetRoot()
+	content := ct[0]
+	cLog.Notice("in scheck" + ct[0])
+
+	if "" == content {
+		ret := echojson(errres)
+		fmt.Fprintf(w, "Catch: %s", ret)
+		return
+	}
+
+	cnt := Root.Query(content)
+
+	res := resp{
+		Errno:  0,
+		Errmsg: "ok",
+		Data:   cnt,
+	}
+
+	ret := echojson(res)
+	//    fmt.Printf("this is :%s", string(ret), res.errmsg, res.errno)
+	fmt.Fprintf(w, "Catch: %s", ret)
+}
 
 func echojson(res resp) []byte {
-    ret,err := json.Marshal(res)
-    if err != nil {
-        ret,err = json.Marshal(errres)
-    }
+	ret, err := json.Marshal(res)
+	if err != nil {
+		ret, err = json.Marshal(errres)
+	}
 
-    return ret
+	return ret
 }
 
-
 //建立tries树
-func insert(str string, root *node) {
-    cur := root
-    str_rune := []rune(str)
-    for i:=0; i<len(str_rune); i++ {
-        chld, ok := cur.sons[str_rune[i]]
-        if ok {
-            cur = chld
-        } else {
-            if nil == cur.sons {
-                cur.sons = make(map[rune] *node, 1000)
-                }
+func insert(str string, Root *node) {
+	cur := Root
+	str_rune := []rune(str)
+	for i := 0; i < len(str_rune); i++ {
+		chld, ok := cur.sons[str_rune[i]]
+		if ok {
+			cur = chld
+		} else {
+			if nil == cur.sons {
+				//cur.sons = make(map[rune]*node, 1000)
+				cur.sons = make(map[rune]*node)
+			}
 
-                n_node := new(node)
-                cur.sons[str_rune[i]] = n_node
-                n_node.nc = str_rune[i]
+			n_node := new(node)
+			cur.sons[str_rune[i]] = n_node
+			n_node.nc = str_rune[i]
 
-                cur = n_node
-            }
-        }
+			cur = n_node
+		}
+	}
 
-        cur.count++;
-//        fmt.Printf("rune type : %c   %d\n", cur.nc, cur.count)
-    }
+	cur.count++
+	cur.word = str_rune
+	//	fmt.Printf("rune type : %c   %s\n", cur.nc, str)
+	//        fmt.Printf("rune type : %c   %d\n", cur.nc, cur.count)
+}
 
 //建立回朔路径
-func buidAcAm(root *node) {
-    var queue  [500000]*node
-    var fail_n *node
+func buidAcAm(Root *node) {
+	var queue [500000]*node
+	var fail_n *node
 
-    root.fail = nil
-    head := 0
-    tail := 0
-    queue[head] = root
-    head++
-    for {
-        if head <= tail {
-            break
-            }
-        tmp := queue[tail]
-        tail++
+	Root.fail = nil
+	head := 0
+	tail := 0
+	queue[head] = Root
+	head++
+	for {
+		if head <= tail {
+			break
+		}
+		tmp := queue[tail]
+		tail++
 
-        if(nil != tmp && nil != tmp.sons) {
-            for ch,t_node := range tmp.sons {
+		if nil != tmp && nil != tmp.sons {
+			for ch, t_node := range tmp.sons {
 
-                if tmp == root {
-                    t_node.fail = root
-//                    fmt.Printf("rune fail root: %c\n", t_node.nc)
-                } else {
-                    fail_n = tmp.fail //父节点的fail
-                    for {
-                        if(nil == fail_n) {
-                            t_node.fail = root
-                            break
-                            }
-                        //子节点的fail，设置为父节点fail的相同自节点
-                        if(nil != fail_n.sons[ch]) {
-                            t_node.fail = fail_n.sons[ch]
-////////////////                            fmt.Printf("rune fail dup: %c\n", ch)
-                            break
-                            }
-                        fail_n = fail_n.fail
-                        }
-                    }
+				if tmp == Root {
+					t_node.fail = Root
+					//                    fmt.Printf("rune fail Root: %c\n", t_node.nc)
+				} else {
+					fail_n = tmp.fail //父节点的fail
+					for {
+						if nil == fail_n {
+							t_node.fail = Root
+							break
+						}
+						//子节点的fail，设置为父节点fail的相同自节点
+						if nil != fail_n.sons[ch] {
+							t_node.fail = fail_n.sons[ch]
+							////////////////                            fmt.Printf("rune fail dup: %c\n", ch)
+							break
+						}
+						fail_n = fail_n.fail
+					}
+				}
 
-                //把子节点加入到处理队列中
-                queue[head] = t_node
-                head++
-            }
-        }
-    }
+				//把子节点加入到处理队列中
+				queue[head] = t_node
+				head++
+			}
+		}
+	}
 
 }
 
 //匹配tries树，是否命中敏感词
-func (root *node) Query(str string) int {
-    cur := root
-    str_rune := []rune(str)
-    var temp *node
-    count := 0
+func (Root *node) Query(str string) string {
+	cur := Root
+	str_rune := []rune(str)
+	var temp *node
+	count := 0
+	var res [][]rune
+	had := 0
 
-    for i:=0; i<len(str_rune); i++ {
-        if nil == cur {
-            cur = root
-            }
+	for i := 0; i < len(str_rune); i++ {
+		if nil == cur {
+			cur = Root
+		}
 
-        //如果当前字符不匹配节点的子节点,则递归fail指针
-        if nil == cur.sons[str_rune[i]] {
-            for {
-                if cur == nil || cur == root || nil != cur.sons[str_rune[i]] {
-                    break
-                    }
+		//如果当前字符不匹配节点的子节点,则递归fail指针
+		if nil == cur.sons[str_rune[i]] {
+			for {
+				if cur == nil || cur == Root || nil != cur.sons[str_rune[i]] {
+					break
+				}
 
-                cur = cur.fail
-                }
-            }
+				cur = cur.fail
+			}
+		}
 
-//     fmt.Printf("debug2 :%c %c\n", str_rune[i], cur)
+		//     fmt.Printf("debug2 :%c %c\n", str_rune[i], cur)
 
-        //计算count
-        if nil != cur.sons[str_rune[i]] {
-            cur = cur.sons[str_rune[i]]
-            }
+		//计算count
+		if nil != cur.sons[str_rune[i]] {
+			cur = cur.sons[str_rune[i]]
+		}
 
-//     fmt.Printf("debug1 :%c\n", cur.nc)
+		//     fmt.Printf("debug1 :%c\n", cur.nc)
 
-        temp = cur
-        if nil != temp {
-            for {
-                if temp == root || nil == temp {
-                    break
-                    }
+		temp = cur
+		if nil != temp {
+			for {
+				if temp == Root || nil == temp {
+					break
+				}
 
-                count += temp.count
-                temp = temp.fail
-                }
-            }
-        }
+				count += temp.count
+				if len(temp.word) > 0 {
+					had = 0
 
-        return count
-    }
+					if len(res) > 0 {
+						for _, stword := range res {
+							if string(stword) == string(temp.word) {
+								had = 1
+							}
+						}
+					}
 
-//逐行读取文件
-func cat(scanner *bufio.Scanner, root *node) error{
+					if 0 == had {
+						res = append(res, temp.word)
+					}
+				}
 
-    for scanner.Scan(){
-//        fmt.Println(scanner.Text())
-        row := scanner.Text()
-        insert(strings.TrimSpace(row), root)
-        /*
-        words := strings.Split(strings.TrimSpace(row), " ")
-        for i:=0; i<len(words); i++ {
-            if("" != strings.TrimSpace(words[i])) {
-                insert(strings.TrimSpace(words[i]), root)
-                }
-            fmt.Println("aa",words[i])    
-        }
-        */
-    }
+				temp = temp.fail
+			}
+		}
+	}
 
-    return scanner.Err()
+	restr := ""
+	var strl []string
+	if len(res) > 0 {
+		for _, word := range res {
+			strl = append(strl, strings.Trim(strconv.QuoteToASCII(string(word)), "\""))
+		}
+		restp, err := json.Marshal(strl)
+		if err != nil {
+			restp, err = json.Marshal(errres)
+		}
+		restr = string(restp)
+
+	}
+
+	return restr
 }
 
-func loadFile(fileName string, root *node) {
-    f,err := os.OpenFile(fileName,os.O_RDONLY,0660)
-    if err != nil{
-        fmt.Fprintf(os.Stderr,"%s err read from %s : %s\n",
-        fileName,fileName,err)
-    }
+//逐行读取文件
+func cat(scanner *bufio.Scanner, Root *node) error {
 
-    cat(bufio.NewScanner(f), root)
-    f.Close()
-    }
+	for scanner.Scan() {
+		//        fmt.Println(scanner.Text())
+		row := scanner.Text()
+		insert(strings.TrimSpace(row), Root)
+		/*
+		   words := strings.Split(strings.TrimSpace(row), " ")
+		   for i:=0; i<len(words); i++ {
+		       if("" != strings.TrimSpace(words[i])) {
+		           insert(strings.TrimSpace(words[i]), Root)
+		           }
+		       fmt.Println("aa",words[i])
+		   }
+		*/
+	}
+
+	return scanner.Err()
+}
+
+func loadFile(fileName string, Root *node) {
+	f, err := os.OpenFile(fileName, os.O_RDONLY, 0660)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s err read from %s : %s\n",
+			fileName, fileName, err)
+	}
+
+	cat(bufio.NewScanner(f), Root)
+	f.Close()
+}
 
 func GetRoot() *node {
-    return root
-        }
+	return Root
+}
 
 func New() *node {
-//    source := []byte("我们俩吃饭是敏感词 他我们 你我们仨 我们仨 我们仨")
+	//    source := []byte("我们俩吃饭是敏感词 他我们 你我们仨 我们仨 我们仨")
 
-    root := new(node)
-    file := string("words.txt")
-    loadFile(file, root)
+	Root = new(node)
+	file := string("words.txt")
+	loadFile(file, Root)
 
+	buidAcAm(Root)
 
-     buidAcAm(root)
+	return Root
+	/*
+		   content := "说不能出现砍死你等恶意词汇"
+		   cnt := query(content, Root)
 
-     return root
-     /*
-     content := "说不能出现砍死你等恶意词汇"
-     cnt := query(content, root)
-
-     fmt.Printf("count :%d\n", cnt)
-     */
-    }
+		   fmt.Printf("count :%d\n", cnt)
+	i/e	*/
+}
--- tiny.go
+++ tiny.go
@@ -1,125 +1,170 @@
 package sensitivecheck
 
 import (
-    "fmt"
-    "strings"
-    "net/http"
-    "net/url"
-    "regexp"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"reflect"
+	"regexp"
+	"strings"
 )
 
-type TinyMux struct{
-    regex *regexp.Regexp
-    params map[int] string
-    handle http.HandlerFunc
+type TinyMux struct {
+	regex      *regexp.Regexp
+	params     map[int]string
+	handle     Handle
+	controller reflect.Type
+	//	handle http.HandlerFunc
 }
 
-type CMux struct{
-    routers []*TinyMux
-    staticdir map[string] string
+type CMux struct {
+	routers   []*TinyMux
+	staticdir map[string]string
 }
 
+type Handle func(ctx *Context)
 
 func (t *CMux) Listen(port string) {
-    print("Listening: %s\n", port[1:])
-    http.ListenAndServe(port, t)
+	print("Listening: %s\n", port[1:])
+	http.ListenAndServe(port, t)
 }
 
 func (c *CMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    proced := 0
-    for _, router := range c.routers {
-        if !router.regex.MatchString(r.URL.Path) {
-            continue;
-        }
+	proced := 0
+	path := "/"
+	pathB := "/"
+	for _, router := range c.routers {
 
-        matches := router.regex.FindStringSubmatch(r.URL.Path)
-        if len(matches[0]) != len(r.URL.Path) {
-            continue;
-        }
+		if "/" != r.URL.Path {
+			path = strings.ToLower(strings.Trim(r.URL.Path, "/"))
+			pathB = path + "/"
+		}
 
-        params := make(map[string]string)
+		fmt.Printf("\n %s %s %s\n", path, pathB, router.regex)
+		if !router.regex.MatchString(path) {
+			if !router.regex.MatchString(pathB) {
+				continue
+			}
 
-        if len(router.params) > 0 {
-            values := r.URL.Query()
-            for i,match := range matches[1:] {
-                values.Add(router.params[i], match)
-                params[router.params[i]] = match
-            }
+			path = pathB
+		}
 
-            r.URL.RawQuery = url.Values(values).Encode() + "&" + r.URL.RawQuery
-            cLog.Notice(r.URL.RawQuery)
-            fmt.Printf("%s", params)
-        }
+		matches := router.regex.FindStringSubmatch(path)
+		if len(matches[0]) != len(path) {
+			continue
+		}
 
-        router.handle.ServeHTTP(w, r)
-        proced = 1
-        break
-/*
-        if(r.URL.Path == UrlPath) {
-            handler.ServeHTTP(w, r)
-            proced = 1
-            break
-        }
-        */
-    }
+		params := make(map[string]string)
 
-    if proced == 0 {
-        for path, dir := range c.staticdir {
-            if strings.HasPrefix(r.URL.Path, path) {
-                file := dir+ "/" + r.URL.Path[len(path):]
-                fmt.Println(file)
-                http.ServeFile(w, r, file)
-                proced = 1
-                break
-            }
-        }
-    }
+		if len(router.params) > 0 {
+			values := r.URL.Query()
+			for i, match := range matches[1:] {
+				values.Add(router.params[i], match)
+				params[router.params[i]] = match
+			}
 
+			r.URL.RawQuery = url.Values(values).Encode()
+			cLog.Notice(r.URL.RawQuery)
+		}
 
-    if(0 == proced) {
-        c.FunctionNotFount(w, r)
-    }
+		ctx := NewContext(params, w, r)
 
-    cLog.Notice(r.RemoteAddr+"\t"+r.RequestURI)
+		Action := "Index"
+		if len(ctx.Request.Form["a"]) > 0 {
+			Action = strings.Title(ctx.Request.Form["a"][0])
+		}
+
+		rc := reflect.New(router.controller)
+
+		in := make([]reflect.Value, 2)
+		in[0] = reflect.ValueOf(ctx)
+		in[1] = reflect.ValueOf(router.controller.Name())
+		InitFun := rc.MethodByName("Init")
+		InitFun.Call(in)
+
+		ActionFun := rc.MethodByName(Action)
+		if !ActionFun.IsValid() {
+			fmt.Printf("error, %s \n", ActionFun)
+			break
+		}
+		inp := make([]reflect.Value, 0)
+		ActionFun.Call(inp)
+
+		proced = 1
+		break
+	}
+
+	if proced == 0 {
+		for path, dir := range c.staticdir {
+			if strings.HasPrefix(r.URL.Path, path) {
+				file := dir + "/" + r.URL.Path[len(path):]
+				fmt.Println(file)
+				http.ServeFile(w, r, file)
+				proced = 1
+				break
+			}
+		}
+	}
+
+	if 0 == proced {
+		c.FunctionNotFount(w, r)
+	}
+
+	cLog.Notice(r.RemoteAddr + "\t" + r.RequestURI)
 }
 
-func (t *CMux)Add(patt string, handler http.HandlerFunc) {
-//    t.hpatt[patt] = handler
+//func (t *CMux) Add(patt string, handler http.HandlerFunc) {
+func (t *CMux) Add(patt string, ci ControllerInterface) {
+	//    t.hpatt[patt] = handler
 
-    parts := strings.Split(patt, "/")
+	parts := strings.Split(patt, "/")
 
-    j := 0
-    params := make(map[int] string)
-    for i,part := range parts {
-        if strings.HasPrefix(part, ":") {
-            start_index := 1
-            expr := "([^/]+)"
+	j := 0
+	params := make(map[int]string)
+	for i, part := range parts {
+		//action的位置不能有非正则表达
+		if i == 2 && part != "" && (!strings.HasPrefix(part, ":")) {
+			fmt.Println("注册%s有错误!Action位置不能有非正则表达式哦!", patt)
+			os.Exit(1)
+		}
 
-            if index := strings.Index(part, "("); index != -1 {
-                expr = part[index:]
-                part = part[:index]
-                }
-            params[j] = part[start_index:]
-            parts[i] = expr
-            j++
-        }
-    }
+		if strings.HasPrefix(part, ":") {
+			start_index := 1
+			expr := "([^/]+)"
+			expr = "([^/].)"
 
-    patt = strings.Join(parts, "/")
-    regex, regexErr := regexp.Compile(patt)
-    if regexErr != nil {
-        panic(regexErr)
-        return
-    }
+			if index := strings.Index(part, "("); index != -1 {
+				expr = part[index:]
+				part = part[:index]
+			}
+			params[j] = part[start_index:]
+			parts[i] = expr
+			j++
 
-    route := &TinyMux{}
-    route.regex = regex
-    route.params = params
-    route.handle = handler
+		}
+	}
 
+	patt = strings.Trim(strings.Join(parts, "/"), "/")
+	if "" == patt {
+		patt = "/"
+	}
 
-    t.routers = append(t.routers, route)
-    fmt.Printf("r: %s\n", t.routers)
+	fmt.Printf("patt: %s\n", patt)
+	regex, regexErr := regexp.Compile(strings.ToLower(patt))
+	if regexErr != nil {
+		panic(regexErr)
+		return
+	}
+
+	cv := reflect.Indirect(reflect.ValueOf(ci)).Type()
+	route := &TinyMux{}
+	route.regex = regex
+	route.params = params
+	route.controller = cv
+
+	t.routers = append(t.routers, route)
+	fmt.Printf("r: %s\n", t.routers)
 }
 
 /*//利用默认路由
@@ -132,26 +177,26 @@ func (t *TinyMux)AddStatic(patt string, handler http.Handler) {
 */
 
 func (t *CMux) AddStatic(path string, dir string) {
-    if 0 == len(t.staticdir) {
-        t.staticdir = make(map[string]string)
-    }
-    t.staticdir[path] = dir
+	if 0 == len(t.staticdir) {
+		t.staticdir = make(map[string]string)
+	}
+	t.staticdir[path] = dir
 }
 
 func Newhttpframe() *CMux {
-    cLog.Notice("this is in http framwork")
-    return &CMux{}
-//    return &TinyMux{ *regexp.Regexp, make(map[int] string), http.HandlerFunc, make(map[string] http.HandlerFunc), make(map[string] string)}
+	cLog.Notice("this is in http framwork")
+	return &CMux{}
+	//    return &TinyMux{ *regexp.Regexp, make(map[int] string), http.HandlerFunc, make(map[string] http.HandlerFunc), make(map[string] string)}
 }
 
 func NewRoute() *CMux {
-    cLog.Notice("this is in http framwork")
-    return &CMux{}
+	cLog.Notice("this is in http framwork")
+	return &CMux{}
 }
 
 func (t *CMux) FunctionNotFount(w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("No Action Has Found")
-    w.WriteHeader(404)
-    err_str := "{\"Errno\":-2,\"Errmsg\":\"error path\",\"Data\":\"0\"}"
-    w.Write([]byte(err_str))
+	fmt.Printf("No Action Has Found")
+	w.WriteHeader(404)
+	err_str := "{\"Errno\":-2,\"Errmsg\":\"error path\",\"Data\":\"0\"}"
+	w.Write([]byte(err_str))
 }
